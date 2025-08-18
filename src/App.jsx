e61194a2688a2634b9b0818786c6d37cceff611f
Jere Mäennenä <jere.maennena@gmail.com>
2025-08-19 00:46:41 +0300

    three

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import { useState, useEffect } from "react";
-import ReactPlayer from "react-player";
+import Header from './components/header.jsx';
+import Footer from './components/footer.jsx';
+import { Skeleton } from "./components/ui/skeleton";
 
 export default function Home() {
   const [indoorData, setIndoorData] = useState({ temperature: null, humidity: null });
@@ -38,33 +40,46 @@ export default function Home() {
   }, []);
 
   return (
-    <main className="flex-1 flex flex-col gap-1 w-full max-w-xl mx-auto items-center sm:items-start py-8 ">
-      <div className="w-full aspect-video rounded-md overflow-hidden shadow mb-2 bg-black flex flex-col justify-center items-center relative">
-        {error === "Videon lataus epäonnistui" ? (
-          <span className="absolute top-3 left-2 bg-red-600 text-xs px-2 py-1 rounded z-10"><span className="font-semibold">OFFLINE</span></span>
-        ) : (
-          <span className="absolute top-3 left-2 bg-green-600 text-xs px-2 py-1 rounded z-10"><span className="font-semibold">LIVE</span></span>
-        )}
-        <ReactPlayer
-          url={videoUrl}
-          playing
-          controls
-          muted
-          width="100%"
-          height="100%"
-          onError={() => setError("Videon lataus epäonnistui")}
-        />
-      </div>
-      <div className="mt-1 w-full">
-        <h2 className="text-xl font-semibold mb-1">Sensoritiedot</h2>
-        {error && <p className="text-red-500 font-semibold mb-1">{error}</p>}
-        <p>
-          <span className="font-semibold">Sisälämpötila:</span> {indoorData.temperature !== null ? `${indoorData.temperature}°C, ${indoorData.humidity}%` : "Ladataan..."}
-        </p>
-        <p>
-          <span className="font-semibold">Ulkolämpötila:</span> {outdoorData.temperature !== null ? `${outdoorData.temperature}°C, ${outdoorData.humidity}%` : "Ladataan..."}
-        </p>
-      </div>
-    </main>
+    <div className="min-h-screen flex flex-col bg-black">
+      <Header />
+      <main className="flex-1 flex flex-col w-full max-w-3xl mx-auto items-center sm:items-start px-4 py-8 pb-20">
+        <div className="w-full aspect-video rounded-md overflow-hidden shadow flex flex-col justify-center items-center relative">
+          {error === "Videon lataus epäonnistui" ? (
+            <span className="absolute top-3 left-2 bg-red-600 text-xs px-2 py-1 rounded z-10"><span className="font-semibold">OFFLINE</span></span>
+          ) : (
+            <span className="absolute top-3 left-2 bg-green-600 text-xs px-2 py-1 rounded z-10"><span className="font-semibold">LIVE</span></span>
+          )}
+          <img
+            src={videoUrl}
+            style={{
+              width: "100%",
+              height: "100%",
+              objectFit: "cover",
+              backgroundColor: "gray",
+            }}
+            onError={() => setError("Videon lataus epäonnistui")}
+          />
+        </div>
+        <div className="mt-1 w-full">
+          <h2 className="text-xl font-semibold mb-1 text-gray-300">Sensoritiedot</h2>
+          {error && <p className="text-red-700 font-semibold mb-1">{error}</p>}
+          <p>
+            <span className="font-semibold text-gray-300">Sisälämpötila:</span> {indoorData.temperature !== null ? (
+              <span>
+                <span className="text-orange-400">{indoorData.temperature}°C</span>, <span className="text-blue-300">{indoorData.humidity}%</span>
+              </span>
+            ) : <Skeleton className="h-[20px] w-[100px] rounded-full inline-block align-middle ml-2" />}
+          </p>
+          <p>
+            <span className="font-semibold text-gray-300">Ulkolämpötila:</span> {outdoorData.temperature !== null ? (
+              <span>
+                <span className="text-orange-400">{outdoorData.temperature}°C</span>, <span className="text-blue-300">{outdoorData.humidity}%</span>
+              </span>
+            ) : <Skeleton className="h-[20px] w-[100px] rounded-full inline-block align-middle ml-2" />}
+          </p>
+        </div>
+      </main>
+      <Footer />
+    </div>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import Header from './components/header.jsx'
-import Footer from './components/footer.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Header />
     <App />
-    <Footer />
   </StrictMode>,
 )
