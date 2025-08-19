d680ac496f2dcf9aab64b0d25360d55a6935c4fc
Jere Mäennenä <jere.maennena@gmail.com>
2025-08-19 21:15:06 +0300

    four

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,30 @@
-import { useState, useEffect } from "react";
+import {
+  useState,
+  useEffect
+} from "react";
 import Header from './components/header.jsx';
 import Footer from './components/footer.jsx';
 import { Skeleton } from "./components/ui/skeleton";
+import { Badge } from "./components/ui/badge";
+import { Card } from "./components/ui/card";
+import {
+  Alert,
+  AlertDescription,
+  AlertTitle,
+} from "./components/ui/alert";
+import { Terminal } from "lucide-react";
 
 export default function Home() {
   const [indoorData, setIndoorData] = useState({ temperature: null, humidity: null });
   const [outdoorData, setOutdoorData] = useState({ temperature: null, humidity: null });
   const [error, setError] = useState(null);
+  const [isTempLoading, setIsTempLoading] = useState(true);
+  const [isLive, setIsLive] = useState(true);
 
   const videoUrl = "/api/stream/video-stream";
 
   async function fetchSensorData() {
+    setIsTempLoading(true);
     try {
       const indoorRes = await fetch("/api/dht/indoor");
       const indoorJson = await indoorRes.json();
@@ -27,9 +41,12 @@ export default function Home() {
         humidity: outdoorJson.humidity.toFixed(1),
         timestamp: outdoorJson.timestamp,
       });
+      setError(null);
     } catch (err) {
       console.error("Virhe sensoreiden haussa:", err);
       setError("Ei saatu yhteyttä sensoreihin.");
+    } finally {
+      setIsTempLoading(false);
     }
   }
 
@@ -44,11 +61,6 @@ export default function Home() {
       <Header />
       <main className="flex-1 flex flex-col w-full max-w-3xl mx-auto items-center sm:items-start px-4 py-8 pb-20">
         <div className="w-full aspect-video rounded-md overflow-hidden shadow flex flex-col justify-center items-center relative">
-          {error === "Videon lataus epäonnistui" ? (
-            <span className="absolute top-3 left-2 bg-red-600 text-xs px-2 py-1 rounded z-10"><span className="font-semibold">OFFLINE</span></span>
-          ) : (
-            <span className="absolute top-3 left-2 bg-green-600 text-xs px-2 py-1 rounded z-10"><span className="font-semibold">LIVE</span></span>
-          )}
           <img
             src={videoUrl}
             style={{
@@ -57,27 +69,79 @@ export default function Home() {
               objectFit: "cover",
               backgroundColor: "gray",
             }}
-            onError={() => setError("Videon lataus epäonnistui")}
+            onError={() => {
+              setError("Videon lataus epäonnistui");
+              setIsLive(false);
+            }}
+            onLoad={() => setIsLive(true)}
           />
         </div>
-        <div className="mt-1 w-full">
-          <h2 className="text-xl font-semibold mb-1 text-gray-300">Sensoritiedot</h2>
-          {error && <p className="text-red-700 font-semibold mb-1">{error}</p>}
-          <p>
-            <span className="font-semibold text-gray-300">Sisälämpötila:</span> {indoorData.temperature !== null ? (
-              <span>
-                <span className="text-orange-400">{indoorData.temperature}°C</span>, <span className="text-blue-300">{indoorData.humidity}%</span>
-              </span>
-            ) : <Skeleton className="h-[20px] w-[100px] rounded-full inline-block align-middle ml-2" />}
-          </p>
-          <p>
-            <span className="font-semibold text-gray-300">Ulkolämpötila:</span> {outdoorData.temperature !== null ? (
-              <span>
-                <span className="text-orange-400">{outdoorData.temperature}°C</span>, <span className="text-blue-300">{outdoorData.humidity}%</span>
-              </span>
-            ) : <Skeleton className="h-[20px] w-[100px] rounded-full inline-block align-middle ml-2" />}
-          </p>
+        <div className="w-full flex mt-2 mb-5">
+          <Badge variant={isLive ? "default" : "destructive"}>
+            {isLive ? "Live" : "Offline"}
+          </Badge>
         </div>
+        <h2 className="text-xl font-semibold mb-1 text-gray-300 px-2 ">Sensoritiedot</h2>
+
+        <Alert variant="destructive" className="mb-4">
+          <Terminal />
+          <AlertTitle>Virhe!</AlertTitle>
+          <AlertDescription>
+            {error},
+          </AlertDescription>
+        </Alert>
+
+        {isTempLoading ? (
+          <div className="w-full flex flex-col gap-2 mt-1">
+            <Card className="w-full bg-neutral-800/80 border-neutral-800">
+              <div className="px-4 pb-4">
+                <p>
+                  <span className="font-semibold text-gray-300">🌡️ Sisälämpötila:</span> <Skeleton className="h-[20px] w-[100px] rounded-full inline-block align-middle ml-2" />
+                </p>
+                <span className="font-semibold text-gray-300">💧 Kosteus:</span> <Skeleton className="h-[20px] w-[100px] rounded-full inline-block align-middle ml-2" />
+              </div>
+            </Card>
+            <Card className="w-full bg-neutral-800/80 border-neutral-800">
+              <div className="px-4 pb-4">
+                <p>
+                  <span className="font-semibold text-gray-300">🌡️ Ulkolämpötila:</span> <Skeleton className="h-[20px] w-[100px] rounded-full inline-block align-middle ml-2" />
+                </p>
+                <span className="font-semibold text-gray-300">💧 Kosteus:</span> <Skeleton className="h-[20px] w-[100px] rounded-full inline-block align-middle ml-2" />
+              </div>
+            </Card>
+          </div>
+        ) : (
+          <>
+            <Card className="mt-1 w-full bg-neutral-800/80 border-neutral-800">
+              <div className="px-4 pb-4">
+                <p>
+                  <span className="font-semibold text-gray-300">🌡️ Sisälämpötila:</span> {indoorData.temperature !== null ? (
+                    <span>
+                      <span className="text-gray-400">{indoorData.temperature}°C</span>, <span className="text-blue-300">{indoorData.humidity}%</span>
+                    </span>
+                  ) : <span className="text-gray-400">-</span>}
+                </p>
+                <span className="font-semibold text-gray-300">💧 Kosteus:</span> {indoorData.humidity !== null ? (
+                  <span className="text-blue-300">{indoorData.humidity}%</span>
+                ) : <span className="text-gray-400">-</span>}
+              </div>
+            </Card>
+            <Card className="mt-1 w-full bg-neutral-800/80 border-neutral-800">
+              <div className="px-4 pb-4">
+                <p>
+                  <span className="font-semibold text-gray-300">🌡️ Ulkolämpötila:</span> {outdoorData.temperature !== null ? (
+                    <span>
+                      <span className="text-gray-400">{outdoorData.temperature}°C</span>,
+                    </span>
+                  ) : <span className="text-gray-400">-</span>}
+                </p>
+                <span className="font-semibold text-gray-300">💧 Kosteus:</span> {outdoorData.humidity !== null ? (
+                  <span className="text-blue-300">{outdoorData.humidity}%</span>
+                ) : <span className="text-gray-400">-</span>}
+              </div>
+            </Card>
+          </>
+        )}
       </main>
       <Footer />
     </div>
--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,9 +1,10 @@
-import React from "react";
+import BirdieLW from "../assets/logo/birdieLW.webp";
 
 export default function Header() {
     return (
         <header className="w-full flex items-center justify-center py-6 bg-black border-b border-gray-800 shadow-lg">
-            <h1 className="text-5xl font-extrabold tracking-tight text-gray-100">
+            <h1 className="text-5xl font-extrabold tracking-tight text-gray-100 flex items-center">
+                <img src={BirdieLW} alt="BeakNet logo" className="inline-block w-12 h-12 mr-2" />
                 BeakNet
             </h1>
             <span className="text-sm font-semibold text-gray-400 ml-2">
