526615143203a775551f92a7d56bd1675b5b34df
Jere Mäennenä <jere.maennena@gmail.com>
2025-08-20 23:54:52 +0300

    10

--- src/App.jsx
+++ src/App.jsx
@@ -56,7 +56,7 @@ export default function Home() {
     return () => clearInterval(interval);
   }, [fetchSensorData]);
 
-  const isMaintenance = false;
+  const isMaintenance = true;
   if (isMaintenance) {
     return <MaintenancePage />;
   }
