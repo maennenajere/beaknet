89534504a2f42afe4f83936b1f7b079d36928856
Jere Mäennenä <jere.maennena@gmail.com>
2025-10-14 23:18:33 +0300

    26

--- src/components/maintenancePage.jsx
+++ src/components/maintenancePage.jsx
@@ -1,5 +1,10 @@
 import React from 'react';
 import BirdieLW from "../assets/logo/birdieLW.webp";
+import {
+    HoverCard,
+    HoverCardContent,
+    HoverCardTrigger,
+} from "@/components/ui/hover-card"
 
 export default function MaintenancePage() {
     return (
@@ -9,8 +14,18 @@ export default function MaintenancePage() {
             <p className="text-xl text-gray-400 mb-8">
                 Sivusto on tilapäisesti huollossa.
                 <br />
-                Palaamme pian!
+                Palaamme pian takaisin linjoille!
             </p>
+            <HoverCard>
+                <HoverCardTrigger className="text-m text-white mb-8 underline underline-offset-4 cursor-pointer">
+                    Mistä oikein on kyse?
+                </HoverCardTrigger>
+                <HoverCardContent className="bg-neutral-900 text-gray-200 p-4 rounded-2xl w-80 sm:w-96 shadow-lg border border-neutral-800">
+                    <p>
+                        Beaky on BeakNet-projektin kurkistusaukko linnunpönttöön, joka on varustettu kameralla ja sensoreilla. Seuraa lintujen elämää ja ympäristöä reaaliajassa.
+                    </p>
+                </HoverCardContent>
+            </HoverCard>
         </div>
     );
 }
